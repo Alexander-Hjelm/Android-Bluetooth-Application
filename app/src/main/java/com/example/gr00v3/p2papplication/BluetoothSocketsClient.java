70b023ccb3e24247977207d659649636639dc110
Alexander Hjelm <admin@alexander-hjelm.com>
2017-03-21 12:34:50 +0100

    Store paired devices internally upon discovery

--- app/src/main/java/com/example/gr00v3/p2papplication/BluetoothSocketsClient.java
+++ app/src/main/java/com/example/gr00v3/p2papplication/BluetoothSocketsClient.java
@@ -4,23 +4,32 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.content.Context;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.ParcelUuid;
 import android.util.Log;
 
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.UUID;
 import java.util.Set;
 
 
 public class BluetoothSocketsClient {
     // Debugging
-    private static final String TAG = "BluetoothChatService";
+    private static final String TAG = "BluetoothSocketsClient";
     private static final boolean D = true;
 
     // Unique UUID for this application
@@ -28,6 +37,9 @@ public class BluetoothSocketsClient {
     // Member fields
     private final Context mContext;
     private final BluetoothAdapter mAdapter;
+
+    //External devices
+    ArrayList<BluetoothDevice> devices = new ArrayList<BluetoothDevice>();
 
     /**
      * Constructor. Prepares a new BluetoothChat session.
@@ -40,16 +52,17 @@ public class BluetoothSocketsClient {
         //mHandler = handler;
     }
 
-    public void DiscoverDevices() {
+    public void DiscoverPairedDevices() {
         // Starting the device discovery
         if (D) Log.d(TAG, "Starting Discovery");
-        mAdapter.startDiscovery();
+        Set<BluetoothDevice> devicesDiscovered = mAdapter.getBondedDevices();
         if (D) Log.d(TAG, "Discovery done");
+
         // Listing paired devices
-        if (D) Log.d(TAG, "Devices paired:");
-        Set<BluetoothDevice> devices = mAdapter.getBondedDevices();
-        for (BluetoothDevice device : devices) {
-            if (D) Log.d(TAG, "Found device: " + device.getName() + " Address: " + device.getAddress());
+        if (D) Log.d(TAG, "Already paired devices:");
+        for (BluetoothDevice device : devicesDiscovered) {
+            if (D) Log.d(TAG, "Found alreday paired device: " + device.getName() + " Address: " + device.getAddress());
+            devices.add(device);
         }
 
     }
